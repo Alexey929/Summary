1b87febec58547ba18fca866d6056385d97d29e3
Alexey929 <thelom05@mail.ru>
2020-04-07 14:43:01 +0600

    toDo list was added

---
+++ toDo/toDo.js
@@ -0,0 +1,43 @@
+let input = document.querySelector("#input");
+let buttonAdd = document.querySelector("#add");
+let list = document.querySelector('#toDoList');
+buttonAdd.onclick = function (){	
+	if (!input.value){
+		alert('input is empty');
+	}
+	else{
+	let action = document.createElement('li');
+	let cancel = document.createElement('button');
+	let check = document.createElement('input');
+	check.type = 'checkbox';
+	check.classList.add('check');
+	cancel.classList.add('cancel');
+	action.classList.add('action');
+	action.textContent = input.value;
+	cancel.textContent = "X";
+	list.append(action);
+	action.append(cancel);
+	action.prepend(check);
+	input.value = '' ;
+
+	buttonAdd.style.background = 'lime';
+	setTimeout(()=> buttonAdd.style.background ='limegreen',200);		
+
+	check.onclick = function(){
+	if(action.style.textDecoration =='') action.style.textDecoration = 'line-through';			
+	else action.style.textDecoration = '';
+	}
+	cancel.onclick = function(){
+	cancel.parentElement.remove();
+}
+	}
+}
+
+function flashingTitle(){
+let title = document.querySelector("h1");
+if (title.style.color == 'darkblue') title.style.color = 'blue';
+else title.style.color = 'darkblue';	
+}
+let timer = setInterval(flashingTitle,500);
+
+
